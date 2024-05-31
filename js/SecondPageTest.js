8f7e7bc826eca783a5826babbd516a2a861fc6e0
Vladimir Moiseev <americandream88@list.ru>
2024-05-31 14:28:05 +0300

    ho-ho-ho

--- js/SecondPageTest.js
+++ js/SecondPageTest.js
@@ -1,5 +1,22 @@
-const basketBtn = document.querySelector('.basket_btn');
+const basketBtn = document.querySelector('.basket_btn'),
+      input = document.querySelector(".input_field"),
+      price = document.querySelector(".lb_price p");
+      let a;
+
+      console.log(price)
 
 basketBtn.addEventListener("click", e => {
     console.log("WORK!")
 })
+
+
+input.addEventListener("input",
+function name(e) {
+   
+    a = input.value + "lb";
+    console.log(a)
+    price.innerText = a;
+    console.log(price.innerText )
+    // price.innerHTML = `<p>${a}</p>`
+})
+
